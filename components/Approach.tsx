c967907715283ddf410e28f85a000a0bdcbd3393
Felixdiamond <diamondfelix006@gmail.com>
2024-09-03 21:23:11 +0100

    chore: Update Approach component with new icons and descriptions

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -3,6 +3,10 @@ import React from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
+import { IoExtensionPuzzle } from "react-icons/io5";
+import { MdDesignServices } from "react-icons/md";
+import { FaCode } from "react-icons/fa6";
+import { HiRocketLaunch } from "react-icons/hi2";
 
 const Approach = () => {
   return (
@@ -12,11 +16,10 @@ const Approach = () => {
       </h1>
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
         <Card
-          title="Planning and Strategy"
-          icon={<AceternityIcon order="Phase 1" />}
-          description="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          title="Discovery & Planning"
+          icon={<IoExtensionPuzzle size={40} />}
+          order="Phase 1"
+          description="I begin by deeply understanding your project goals, target audience, and technical requirements. This phase involves collaborative brainstorming, defining project scope, and creating a detailed roadmap."
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -24,11 +27,10 @@ const Approach = () => {
           />
         </Card>
         <Card
-          title="Development and Progress Update"
-          icon={<AceternityIcon order="Phase 2" />}
-          description="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          title="Design & Prototyping"
+          icon={<MdDesignServices size={40} />}
+          order="Phase 2"
+          description="With a clear plan in place, I move on to designing intuitive user interfaces and creating interactive prototypes. This iterative process ensures we nail down the user experience before diving into development."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -42,15 +44,26 @@ const Approach = () => {
           <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
         </Card>
         <Card
-          title="Development and Launch"
-          icon={<AceternityIcon order="Phase 3" />}
-          description="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          title="Development & Testing"
+          icon={<FaCode size={40} />}
+          order="Phase 3"
+          description="This is where your project comes to life. I write clean, efficient code for both frontend and backend, continuously testing to ensure functionality, performance, and security meet the highest standards."
         >
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-sky-600"
+            colors={[[125, 211, 252]]}
+          />
+        </Card>
+        <Card
+          title="Deployment & Maintenance"
+          icon={<HiRocketLaunch size={40} />}
+          order="Phase 4"
+          description="Once development is complete, I handle the deployment process, ensuring your project launches smoothly. I also provide ongoing support and maintenance to keep your application running flawlessly."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-purple-900"
             colors={[[125, 211, 252]]}
           />
         </Card>
@@ -62,11 +75,13 @@ const Approach = () => {
 const Card = ({
   title,
   icon,
+  order,
   description,
   children,
 }: {
   title: string;
   icon: React.ReactNode;
+  order: string;
   description: string;
   children?: React.ReactNode;
 }) => {
@@ -75,13 +90,8 @@ const Card = ({
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] lg:h-[35rem] rounded-lg"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] lg:h-[35rem] rounded-[1rem]"
     >
-      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
-
       <AnimatePresence>
         {hovered && (
           <motion.div
@@ -96,12 +106,20 @@ const Card = ({
 
       <div className="relative z-20">
         <div className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
-          {icon}
+          <div className="flex items-center justify-center flex-col gap-5 text-purple">
+            {icon}
+            <span className="text-xl md:text-2xl lg:text:3xl font-semibold text-white">
+              {order}
+            </span>
+          </div>
         </div>
         <h2 className="text-center text-2xl md:text-3xl lg:text-4xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
           {title}
         </h2>
-        <h2 className="text-sm opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center" style={{ color: '#e4ecff' }}>
+        <h2
+          className="text-sm opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center"
+          style={{ color: "#e4ecff" }}
+        >
           {description}
         </h2>
       </div>
