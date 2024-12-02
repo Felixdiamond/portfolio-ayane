45d1beb976fcae736120d54ab394f4ae841af9ee
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2024-12-02 15:49:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,35 @@
 "use client";
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
 
-// import preloader using next dynamic import
+// Dynamically import Preloader with ssr: false
 import dynamic from 'next/dynamic';
-const Preloader = dynamic(() => import('@/components/Preloader'), {ssr: false});
+const Preloader = dynamic(() => import('@/components/Preloader'), { ssr: false });
 
-const Hero = lazy(() => import('@/components/Hero'));
-const Grid = lazy(() => import('@/components/Grid'));
-const RecentProjects = lazy(() => import('@/components/Projects'));
-const Testimonials = lazy(() => import('@/components/Testimonials'));
-const Experience = lazy(() => import('@/components/Experience'));
-const Approach = lazy(() => import('@/components/Approach'));
-const Footer = lazy(() => import('@/components/Footer'));
-const Socials = lazy(() => import('@/components/Socials'));
+// Lazy load the rest of the components
+const Hero = dynamic(() => import('@/components/Hero'), { ssr: false });
+const Grid = dynamic(() => import('@/components/Grid'), { ssr: false });
+const RecentProjects = dynamic(() => import('@/components/Projects'), { ssr: false });
+const Testimonials = dynamic(() => import('@/components/Testimonials'), { ssr: false });
+const Experience = dynamic(() => import('@/components/Experience'), { ssr: false });
+const Approach = dynamic(() => import('@/components/Approach'), { ssr: false });
+const Footer = dynamic(() => import('@/components/Footer'), { ssr: false });
+const Socials = dynamic(() => import('@/components/Socials'), { ssr: false });
 
 export default function Home() {
+  // Prevents issues during SSR
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // This will run only on the client side, after the first render
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <Preloader />;
+  }
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx:auto sm:px-10 px-5 overflow-clip">
       <FloatingNav navItems={navItems} />
